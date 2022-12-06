090ff140118eba1a5a989baf4f5c8fa02e420a1a
Herman <herman@q42.nl>
2022-12-06 07:44:30 +0100

    day6 part1

---
+++ day6/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"aoc/lib"
+	"fmt"
+	"io"
+	"sort"
+	"unicode"
+)
+
+func main() {
+	r := lib.Reader()
+	defer r.Close()
+
+	var buf []byte = make([]byte, 4)
+	var i = 0
+	var waitingForEndOfLine = false
+	for {
+		read, err := r.Read(buf[0:1])
+		if read == 0 || err == io.EOF {
+			break
+		}
+		if waitingForEndOfLine && !unicode.IsSpace(rune(buf[0])) {
+			continue
+		}
+		if unicode.IsSpace(rune(buf[0])) {
+			i = 0
+			waitingForEndOfLine = false
+			fmt.Println("")
+			continue
+		}
+		fmt.Print(string(buf[0]))
+		i += read
+		if i > 3 && unique(buf) {
+			waitingForEndOfLine = true
+			fmt.Println(" ", i)
+		}
+		copy(buf[1:4], buf[0:3])
+	}
+}
+
+var room []byte
+
+func unique(buf []byte) bool {
+	if len(room) < len(buf) {
+		room = make([]byte, len(buf))
+	}
+	copy(room, buf)
+
+	sort.Sort(byteSort(room))
+	var last byte
+	for i, next := range room[0:len(buf)] {
+		if i > 0 && next == last {
+			return false
+		}
+		last = next
+	}
+	return true
+}
+
+type byteSort []byte
+
+// Len implements sort.Interface
+func (bs byteSort) Len() int {
+	return len(bs)
+}
+
+// Less implements sort.Interface
+func (bs byteSort) Less(i int, j int) bool {
+	return bs[i] < bs[j]
+}
+
+// Swap implements sort.Interface
+func (bs byteSort) Swap(i int, j int) {
+	var tmp = bs[j]
+	bs[j] = bs[i]
+	bs[i] = tmp
+}
+
+var _ sort.Interface = byteSort([]byte{})

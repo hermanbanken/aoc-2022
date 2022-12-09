0822b236262c538d01d2e8b1f1b7dcc91e1076db
Herman <herman@q42.nl>
2022-12-09 07:48:53 +0100

    control lib

---
+++ lib/control.go
@@ -0,0 +1,24 @@
+package lib
+
+// Ternary short-hand
+func Ternary[T any](condition bool, a, b T) T {
+	if condition {
+		return a
+	}
+	return b
+}
+
+// ErrOnly discards any response value except the error
+func ErrOnly(_ interface{}, err error) error {
+	return err
+}
+
+// ErrOnly2 discards any response value except the error
+func ErrOnly2(_, _ interface{}, err error) error {
+	return err
+}
+
+// ErrOnly3 discards any response value except the error
+func ErrOnly3(_, _, _ interface{}, err error) error {
+	return err
+}

5f7a879abf945b2260e2ecbfd66163c3d5aacd10
Herman <herman@q42.nl>
2022-12-09 07:18:56 +0100

    day9 part2

--- day9/main.go
+++ day9/main.go
@@ -9,13 +9,58 @@ import (
 	"strings"
 )
 
+type pos struct {
+	x, y int
+}
+
+func (p *pos) add(o pos) {
+	p.x += o.x
+	p.y += o.y
+}
+func (p pos) diff(o pos) int {
+	return diff(p.x, o.x) + diff(p.y, o.y)
+}
+func (p pos) diag(o pos) bool {
+	return diff(p.x, o.x) > 0 && diff(p.y, o.y) > 0
+}
+func (p pos) dir(o pos) (r pos) {
+	if p.x > o.x {
+		r.x = 1
+	} else if o.x > p.x {
+		r.x = -1
+	}
+	if p.y > o.y {
+		r.y = 1
+	} else if o.y > p.y {
+		r.y = -1
+	}
+	return
+}
+
+type rope []pos
+
+func (myrope rope) move(direction string) {
+	m := move(direction)
+	myrope[0].add(m)
+	for i := 1; i < len(myrope); i++ {
+		// follow
+		d := myrope[i].diff(myrope[i-1])
+		if d < 2 || (d <= 2 && myrope[i].diag(myrope[i-1])) {
+			// nothing
+		} else {
+			// normal move & diagonal
+			myrope[i].add(myrope[i-1].dir(myrope[i]))
+		}
+	}
+}
+
 func main() {
 	r := lib.Reader()
 	defer r.Close()
 	scanner := bufio.NewScanner(r)
 
 	// Read stacks
-	var hx,hy,tx,ty int
+	var myrope rope = make([]pos, 10)
 	var visited Grid = Grid{}
 
 	var moves []string
@@ -24,34 +69,18 @@ func main() {
 		moves = append(moves, t)
 	}
 
-	visited.Visit(tx, ty)
+	visited.Visit(myrope[9])
 	for _, t := range moves {
 		p := strings.Split(t, " ")
 		direction := p[0]
 		amount, _ := strconv.Atoi(p[1])
 		fmt.Println(direction, amount)
 		for i := 0; i < amount; i++ {
-			fmt.Println(hx, hy, tx, ty, " diff ", diff(hx, tx)+ diff(hy, ty))
-			mx,my := move(direction)
-			hx += mx
-			hy += my
-	
-			// follow
-			d := diff(hx, tx) + diff(hy, ty)
-			if d < 2 || (d <= 2 && diff(hx, tx) > 0 && diff(hy, ty) > 0) {
-				// nothing
-			} else {
-				// normal move & diagonal
-				tmx, tmy := dir(hx, hy, tx, ty)
-				tx += tmx
-				ty += tmy
-			}
-			visited.Visit(tx, ty)
+			fmt.Println(myrope)
+			myrope.move(direction)
+			visited.Visit(myrope[9])
 		}
 	}
-	fmt.Println("last")
-	fmt.Println(hx, hy, tx, ty, "diff", diff(hx, tx)+ diff(hy, ty))
-
 	fmt.Println(visited.Count())
 
 	if err := scanner.Err(); err != nil {
@@ -65,16 +94,16 @@ type Grid struct {
 
 func diff(a, b int) int {
 	if a > b {
-		return a-b
+		return a - b
 	}
-	return b-a
+	return b - a
 }
 
-func (g *Grid) Visit(x,y int) {
+func (g *Grid) Visit(p pos) {
 	for g.data == nil {
 		g.data = make(map[string]bool)
 	}
-	g.data[strconv.Itoa(y)+","+strconv.Itoa(x)] = true
+	g.data[strconv.Itoa(p.y)+","+strconv.Itoa(p.x)] = true
 	return
 }
 func (g Grid) Count() (count int) {
@@ -84,20 +113,22 @@ func (g Grid) Count() (count int) {
 	return
 }
 
-func move (dir string) (x, y int) {
+func move(dir string) pos {
 	switch dir {
-	case "R": return 1, 0
-	case "U": return 0, -1
-	case "L": return -1, 0
-	case "D": return 0, 1
+	case "R":
+		return pos{1, 0}
+	case "U":
+		return pos{0, -1}
+	case "L":
+		return pos{-1, 0}
+	case "D":
+		return pos{0, 1}
 	}
 	log.Fatal(dir)
-	return 0, 0
+	return pos{0, 0}
 }
 
-
-
-func dir (ax, ay, bx, by int) (x, y int) {
+func dir(ax, ay, bx, by int) (x, y int) {
 	if ax > bx {
 		x = 1
 	} else if bx > ax {

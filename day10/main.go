ed577d816eb8245875d171b968f9d4f246ca472a
Herman <herman@q42.nl>
2022-12-10 06:36:44 +0100

    part2

--- day10/main.go
+++ day10/main.go
@@ -3,27 +3,12 @@ package main
 import (
 	"aoc/lib"
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
-
-type rope []lib.Coord
-
-func (myrope rope) move(m lib.Coord) {
-	myrope[0].Add(m)
-	for i := 1; i < len(myrope); i++ {
-		// follow
-		d := myrope[i].Diff(myrope[i-1])
-		if d < 2 || (d <= 2 && myrope[i].Diag(myrope[i-1])) {
-			// nothing
-		} else {
-			// normal move & diagonal
-			myrope[i].Add(myrope[i-1].Dir(myrope[i]))
-		}
-	}
-}
 
 func main() {
 	r := lib.Reader()
@@ -33,10 +18,18 @@ func main() {
 	cycle := 1
 	x := 1
 	var values []int
+	var crt []byte = bytes.Repeat([]byte{' '}, 40)
 	check := func() {
 		if cycle == 20 || (cycle-20)%40 == 0 {
-			fmt.Println(cycle, x)
 			values = append(values, cycle*x)
+		}
+		if cycle > 0 && cycle%40 == 1 {
+			fmt.Println(string(crt))
+			crt = bytes.Repeat([]byte{' '}, 40)
+		}
+		crtPos := (cycle - 1) % 40
+		if x-1 == crtPos || x == crtPos || x+1 == crtPos {
+			crt[crtPos] = '#'
 		}
 	}
 

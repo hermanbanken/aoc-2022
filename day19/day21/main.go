c17233b423261649cd0fc071c08b2a2f7a1f8a74
Herman <herman@q42.nl>
2022-12-21 07:30:13 +0100

    cleanup

--- day19/day21/main.go
+++ day19/day21/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"aoc/lib"
 	"log"
-	"math/big"
 	"strings"
 )
 
@@ -11,23 +10,17 @@ type Monkey struct {
 	op    string
 	elemA string
 	elemB string
-	sum   *big.Int
+	sum   int64
 }
 
-func (m Monkey) Sum() *big.Int {
-	if m.sum != nil {
+func (m Monkey) A() Monkey { return monkeys[m.elemA] }
+func (m Monkey) B() Monkey { return monkeys[m.elemB] }
+
+func (m Monkey) Sum() int64 {
+	if m.sum != 0 {
 		return m.sum
 	}
-	a := monkeys[m.elemA].Sum()
-	b := monkeys[m.elemB].Sum()
-	return op(m.op, a, b)
-}
-
-func (m Monkey) A() Monkey {
-	return monkeys[m.elemA]
-}
-func (m Monkey) B() Monkey {
-	return monkeys[m.elemB]
+	return op(m.op, m.A().Sum(), m.B().Sum())
 }
 
 func (m Monkey) HasHuman(name string) bool {
@@ -37,77 +30,69 @@ func (m Monkey) HasHuman(name string) bool {
 	if m.elemA == "" {
 		return false
 	}
-	return monkeys[m.elemA].HasHuman(m.elemA) || monkeys[m.elemB].HasHuman(m.elemB)
+	return m.A().HasHuman(m.elemA) || m.B().HasHuman(m.elemB)
 }
 
-func (m Monkey) MakeEqualTo(nr *big.Int) (human *big.Int) {
+func (m Monkey) MakeEqualTo(nr int64) (human int64) {
 	if m.elemA == "" {
 		log.Println("Human", m, nr)
 		return nr
 	}
 
-	log.Println("MakeEqualTo", nr, "(", m.elemA, m.op, m.elemB, ")")
+	log.Printf("MakeEqualTo: %d == %s %s %s\n", nr, m.elemA, m.op, m.elemB)
+	var a, b int64
 	if m.A().HasHuman(m.elemA) {
-		fixed := m.B().Sum()
-		dynamic := inverseOp(m.op, nr, nil, fixed)
-		if op(m.op, dynamic, fixed).Cmp(nr) != 0 {
-			log.Fatal("a math doesnt work out ", op(m.op, dynamic, fixed), "!=", nr, " || ", nr, "==", "[", dynamic, "]", m.op, fixed)
-		}
-		human = m.A().MakeEqualTo(dynamic)
+		b = m.B().Sum()
+		a = inverseOp(m.op, nr, b, true)
 	} else {
-		fixed := m.A().Sum()
-		dynamic := inverseOp(m.op, nr, fixed, nil)
-		if op(m.op, fixed, dynamic).Cmp(nr) != 0 {
-			log.Fatal("b math doesnt work out ", op(m.op, fixed, dynamic), "!=", nr, " || ", nr, "==", fixed, m.op, "[", dynamic, "]")
-		}
-		human = m.B().MakeEqualTo(dynamic)
+		a = m.A().Sum()
+		b = inverseOp(m.op, nr, a, false)
 	}
-	return human
-}
+	if op(m.op, a, b) != nr {
+		log.Fatalf(lib.Ternary(m.A().HasHuman(m.elemA),
+			"math doesnt work out %d != %d (math: %d == [%d] %s %d)",
+			"math doesnt work out %d != %d (math: %d == %d %s [%d])"),
+			op(m.op, a, b), nr, nr, a, m.op, b)
+	}
 
-func inverseOp(name string, result *big.Int, componentA, componentB *big.Int) (out *big.Int) {
-	out = big.NewInt(0)
-	component := lib.Ternary(componentA == nil, componentB, componentA)
-	if componentB == nil {
-		switch name {
-		case "*":
-			return out.Div(result, component)
-		case "+":
-			return out.Sub(result, component)
-		case "/":
-			return out.Mul(result, component)
-		case "-":
-			return out.Sub(component, result)
-		default:
-			panic("unknown op: " + name)
-		}
+	if m.A().HasHuman(m.elemA) {
+		return m.A().MakeEqualTo(a)
 	} else {
-		switch name {
-		case "*":
-			return out.Div(result, component)
-		case "+":
-			return out.Sub(result, component)
-		case "/":
-			return out.Mul(result, component)
-		case "-":
-			return out.Add(result, component)
-		default:
-			panic("unknown op: " + name)
-		}
+		return m.B().MakeEqualTo(b)
 	}
 }
 
-func op(name string, a, b *big.Int) (out *big.Int) {
-	out = big.NewInt(0)
+func inverseOp(name string, result int64, component int64, calcLeftComponent bool) (out int64) {
 	switch name {
 	case "*":
-		return out.Mul(a, b)
+		return result / component
 	case "+":
-		return out.Add(a, b)
+		return result - component
 	case "/":
-		return out.Div(a, b)
+		if calcLeftComponent {
+			return result * component
+		}
+		return component / result
 	case "-":
-		return out.Sub(a, b)
+		if !calcLeftComponent {
+			return component - result
+		}
+		return result + component
+	default:
+		panic("unknown op: " + name)
+	}
+}
+
+func op(name string, a, b int64) (out int64) {
+	switch name {
+	case "*":
+		return (a * b)
+	case "+":
+		return (a + b)
+	case "/":
+		return (a / b)
+	case "-":
+		return (a - b)
 	default:
 		panic("unknown op: " + name)
 	}
@@ -124,19 +109,15 @@ func main() {
 			if lib.Int(line[6:]) == 0 {
 				panic("0 in input")
 			}
-			monkeys[line[0:4]] = Monkey{sum: big.NewInt(lib.Int64(line[6:]))}
+			monkeys[line[0:4]] = Monkey{sum: lib.Int64(line[6:])}
 		}
 	})
 
 	log.Println("partA", monkeys["root"].Sum())
 
-	var humn = big.NewInt(0)
 	if monkeys["root"].A().HasHuman(monkeys["root"].elemA) {
-		humn = monkeys["root"].A().MakeEqualTo(monkeys["root"].B().Sum())
+		log.Println("partB", monkeys["root"].A().MakeEqualTo(monkeys["root"].B().Sum()))
 	} else {
-		humn = monkeys["root"].B().MakeEqualTo(monkeys["root"].A().Sum())
+		log.Println("partB", monkeys["root"].B().MakeEqualTo(monkeys["root"].A().Sum()))
 	}
-	// not 8112670909931
-	// answer is 3423279932937
-	log.Println("partB", humn)
 }

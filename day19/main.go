acbb11b1751081b6e8ba4fb96e9f202d5a42ebe1
Herman <herman@q42.nl>
2022-12-21 08:03:58 +0100

    day19: convert to arrays

--- day19/main.go
+++ day19/main.go
@@ -27,61 +27,79 @@ type BP struct {
 	// Each geode robot costs 2 ore and 17 obsidian.
 }
 
+type Mineral int
+
+const (
+	Ore      = Mineral(0)
+	Clay     = Mineral(1)
+	Obsidian = Mineral(2)
+	Geode    = Mineral(3)
+)
+
 type State struct {
 	Previous *State
 	T        int
 
-	OreRobot      int
-	ClayRobot     int
-	ObsidianRobot int
-	GeodeRobot    int
-
-	Ore      int
-	Clay     int
-	Obsidian int
-	Geode    int
+	Robots   [4]int
+	Minerals [4]int
 }
 
 func (s State) Less(other State) bool {
-	if s.GeodeRobot != other.GeodeRobot {
-		return s.GeodeRobot < other.GeodeRobot
-	}
-	if s.ObsidianRobot != other.ObsidianRobot {
-		return s.ObsidianRobot < other.ObsidianRobot
-	}
-	if s.ClayRobot != other.ClayRobot {
-		return s.ClayRobot < other.ClayRobot
-	}
-	if s.OreRobot != other.OreRobot {
-		return s.OreRobot < other.OreRobot
-	}
-	return false
+	return s != other &&
+		s.Robots[Ore] <= other.Robots[Ore] &&
+		s.Minerals[Ore] <= other.Minerals[Ore] &&
+		s.Robots[Clay] <= other.Robots[Clay] &&
+		s.Minerals[Clay] <= other.Minerals[Clay] &&
+		s.Robots[Obsidian] <= other.Robots[Obsidian] &&
+		s.Minerals[Obsidian] <= other.Minerals[Obsidian] &&
+		s.Robots[Geode] <= other.Robots[Geode] &&
+		s.Minerals[Geode] <= other.Minerals[Geode]
+	// if s.GeodeRobot != other.GeodeRobot {
+	// 	return s.GeodeRobot < other.GeodeRobot
+	// }
+	// if s.ObsidianRobot != other.ObsidianRobot {
+	// 	return s.ObsidianRobot < other.ObsidianRobot
+	// }
+	// if s.ClayRobot != other.ClayRobot {
+	// 	return s.ClayRobot < other.ClayRobot
+	// }
+	// if s.OreRobot != other.OreRobot {
+	// 	return s.OreRobot < other.OreRobot
+	// }
+	// return false
 }
 
 func (s State) MineMineralsInto(bp BP) func(dest State) State {
 	return func(dest State) State {
-		dest.T++
-		dest.Previous = &s
-		dest.Ore += s.OreRobot
-		dest.Clay += s.ClayRobot
-		dest.Obsidian += s.ObsidianRobot
-		dest.Geode += s.GeodeRobot
+		return dest.Mod(func(dest *State) {
+			dest.T++
+			dest.Previous = &s
+			dest.Minerals[Ore] += s.Robots[Ore]
+			dest.Minerals[Clay] += s.Robots[Clay]
+			dest.Minerals[Obsidian] += s.Robots[Obsidian]
+			dest.Minerals[Geode] += s.Robots[Geode]
 
-		// Set dest to INFINITY (maxInt) if were never running out again
-		if dest.Ore >= (minutes-dest.T-1)*max(bp.OreRobotOre, bp.ClayRobotOre, bp.ObsidianRobotOre, bp.GeodeRobotOre) {
-			dest.Ore = lib.Infinity
-		}
-		if dest.Clay >= (minutes-dest.T-1)*bp.ObsidianRobotClay {
-			dest.Clay = lib.Infinity
-		}
-		if dest.Obsidian >= (minutes-dest.T-1)*bp.GeodeRobotObsidian {
-			dest.Obsidian = lib.Infinity
-		}
-		return dest
+			// Set dest to INFINITY (maxInt) if were never running out again
+			if dest.Minerals[Ore] >= (minutes-dest.T-1)*max(bp.OreRobotOre, bp.ClayRobotOre, bp.ObsidianRobotOre, bp.GeodeRobotOre) {
+				dest.Minerals[Ore] = lib.Infinity
+			}
+			if dest.Minerals[Clay] >= (minutes-dest.T-1)*bp.ObsidianRobotClay {
+				dest.Minerals[Clay] = lib.Infinity
+			}
+			if dest.Minerals[Obsidian] >= (minutes-dest.T-1)*bp.GeodeRobotObsidian {
+				dest.Minerals[Obsidian] = lib.Infinity
+			}
+		})
 	}
 }
 
 func (s State) Mod(fn func(*State)) State {
+	minerals := [4]int{}
+	robots := [4]int{}
+	copy(minerals[:], s.Minerals[:])
+	copy(robots[:], s.Robots[:])
+	s.Minerals = minerals
+	s.Robots = robots
 	fn(&s)
 	return s
 }
@@ -90,7 +108,7 @@ func (s State) String() (out string) {
 	if s.Previous != nil {
 		out = s.Previous.String() + " -> \n"
 	}
-	out += fmt.Sprintf("%#v (%d)", s, s.Geode)
+	out += fmt.Sprintf("%#v (%d)", s, s.Minerals[Geode])
 	return out
 }
 
@@ -141,32 +159,31 @@ func (s State) Build(bp BP) (out []State) {
 	// 	fmt.Println("]")
 	// }()
 
-	if s.Ore >= bp.OreRobotOre && s.Ore < lib.Infinity {
+	if s.Minerals[Ore] >= bp.OreRobotOre && s.Minerals[Ore] < lib.Infinity {
 		out = append(out, s.Mod(func(s *State) {
-			s.Ore -= bp.OreRobotOre
-			s.OreRobot += 1
-		}) /*.Build(bp)...*/)
+			s.Minerals[Ore] -= bp.OreRobotOre
+			s.Robots[Ore] += 1
+		}))
 	}
-	if s.Ore >= bp.ClayRobotOre && s.Clay < lib.Infinity {
+	if s.Minerals[Ore] >= bp.ClayRobotOre && s.Minerals[Clay] < lib.Infinity {
 		out = append(out, s.Mod(func(s *State) {
-			s.Ore -= bp.ClayRobotOre
-			s.ClayRobot += 1
-		}) /*.Build(bp)...*/)
+			s.Minerals[Ore] -= bp.ClayRobotOre
+			s.Robots[Clay] += 1
+		}))
 	}
-	if s.Ore >= bp.ObsidianRobotOre && s.Clay >= bp.ObsidianRobotClay && s.Obsidian < lib.Infinity {
+	if s.Minerals[Ore] >= bp.ObsidianRobotOre && s.Minerals[Clay] >= bp.ObsidianRobotClay && s.Minerals[Obsidian] < lib.Infinity {
 		out = append(out, s.Mod(func(s *State) {
-			s.Ore -= bp.ObsidianRobotOre
-			s.Clay -= bp.ObsidianRobotClay
-			s.ObsidianRobot += 1
-		}) /*.Build(bp)...*/)
+			s.Minerals[Ore] -= bp.ObsidianRobotOre
+			s.Minerals[Clay] -= bp.ObsidianRobotClay
+			s.Robots[Obsidian] += 1
+		}))
 	}
-	//TODO why not adding geodes bots?
-	if s.Ore >= bp.GeodeRobotOre && s.Obsidian >= bp.GeodeRobotObsidian && s.T < minutes-2 {
+	if s.Minerals[Ore] >= bp.GeodeRobotOre && s.Minerals[Obsidian] >= bp.GeodeRobotObsidian && s.T < minutes-2 {
 		out = append(out, s.Mod(func(s *State) {
-			s.Ore -= bp.GeodeRobotOre
-			s.Obsidian -= bp.GeodeRobotObsidian
-			s.GeodeRobot += 1
-		}) /*.Build(bp)...*/)
+			s.Minerals[Ore] -= bp.GeodeRobotOre
+			s.Minerals[Obsidian] -= bp.GeodeRobotObsidian
+			s.Robots[Geode] += 1
+		}))
 	}
 
 	out = lib.UniqueUsingKey(append(out, s))
@@ -213,10 +230,10 @@ func main() {
 }
 
 func maxGeodes(bp BP) int {
-	states := []State{{T: 0, OreRobot: 1}}
+	states := []State{{T: 0, Robots: [4]int{1, 0, 0, 0}}}
 	max := 0
 	var maxState State
-	for t := 0; t < 20; t++ {
+	for t := 0; t < minutes; t++ {
 		newstates := []State{}
 		for _, oldstate := range states {
 			newstates = append(newstates, lib.Map(oldstate.Build(bp), oldstate.MineMineralsInto(bp))...)
@@ -224,16 +241,16 @@ func maxGeodes(bp BP) int {
 		states = lib.UniqueUsingKey(newstates)
 		max = 0
 		for _, state := range newstates {
-			if max < state.Geode {
-				max = state.Geode
+			if max < state.Minerals[Geode] {
+				max = state.Minerals[Geode]
 				maxState = state
 			}
 		}
 		fmt.Println("t", t, "states", len(states), "max", max)
-		sort.Sort(StateSlice(states))
-		// if len(states) > 20 {
-		// 	states = states[0:20]
-		// }
+		sort.Stable(sort.Reverse(StateSlice(states)))
+		if len(states) > 20 {
+			states = states[0:20]
+		}
 	}
 	fmt.Println(maxState)
 	return max

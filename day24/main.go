0aa8a36ea99775e6b8a1201fc7c6aff91d0167b2
Herman <herman@q42.nl>
2022-12-24 08:00:22 +0100

    day24 part1

---
+++ day24/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"aoc/lib"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Blizzard struct {
+	pos int
+	dir int
+}
+
+func main() {
+	lines := lib.Lines()
+	g := lib.Grid{H: len(lines), W: len(lines[0])}
+	cells := strings.Join(lines, "")
+	blizz := parseBlizzards(g, cells)
+	bm := blizzMap(blizz)
+
+	start := 1
+	end := len(cells) - 2
+	t := 0
+
+	empty := func(p int) bool {
+		if p == end {
+			fmt.Printf("done in %d minutes\n", t+1)
+			os.Exit(0)
+		}
+		if p == start || p == end {
+			return true
+		}
+		if p%g.W == 0 || p%g.W == g.W-1 {
+			return false
+		}
+		if p/g.W == 0 || p/g.W == g.H-1 {
+			return false
+		}
+		_, hasBlizzard := bm[p]
+		return !hasBlizzard
+	}
+
+	moveBlizzard := func(b Blizzard) Blizzard {
+		np := b.pos + b.dir
+		if np%g.W == 0 {
+			np += g.W - 2
+		}
+		if np%g.W == g.W-1 {
+			np -= g.W - 2
+		}
+		if np/g.W == 0 {
+			np += g.W * (g.H - 2)
+		}
+		if np/g.W == g.H-1 {
+			np -= g.W * (g.H - 2)
+		}
+		return Blizzard{np, b.dir}
+	}
+
+	g.CanMove = func(posA, posB int) bool { return empty(posB) }
+
+	cycle := (g.W - 2) * (g.H - 2)
+	fmt.Println("cycle", cycle)
+
+	pos := []int{start}
+	minTime := map[int]int{}
+	for ; t < 1000; t++ {
+		fmt.Println("Minute", t+1, len(pos))
+		blizz = lib.Map(blizz, moveBlizzard)
+		bm = blizzMap(blizz)
+
+		newPos := []int{}
+		for _, p := range pos {
+			if empty(p) {
+				newPos = append(newPos, p)
+			}
+			newPos = append(newPos, g.Moves(p)...)
+		}
+		remainingPos := []int{}
+		for _, np := range newPos {
+			if t > 0 && (minTime[np] < t-cycle || minTime[np] == t) {
+				continue
+			}
+			remainingPos = append(remainingPos, np)
+			minTime[np] = t
+		}
+		pos = remainingPos
+		if cycle == 25 {
+			visualize(g, bm, remainingPos)
+		}
+	}
+}
+
+func parseBlizzards(g lib.Grid, cells string) (out []Blizzard) {
+	for i, char := range cells {
+		switch char {
+		case '>':
+			out = append(out, Blizzard{i, 1})
+		case '<':
+			out = append(out, Blizzard{i, -1})
+		case '^':
+			out = append(out, Blizzard{i, -g.W})
+		case 'v':
+			out = append(out, Blizzard{i, g.W})
+		}
+	}
+	return
+}
+
+func blizzMap(bs []Blizzard) (out map[int][]Blizzard) {
+	out = map[int][]Blizzard{}
+	for _, b := range bs {
+		out[b.pos] = append(out[b.pos], b)
+	}
+	return out
+}
+
+func visualize(g lib.Grid, bm map[int][]Blizzard, pos []int) {
+	exp := map[int]bool{}
+	for _, p := range pos {
+		exp[p] = true
+	}
+
+	sb := strings.Builder{}
+	for y := 0; y < g.H; y++ {
+		for x := 0; x < g.W; x++ {
+			p := y*g.W + x
+			c := len(bm[p])
+			if (x == 0 || x == g.W-1 || y == 0 || y == g.H-1) && p != 1 && p != g.W*g.H-2 {
+				sb.WriteRune('#')
+			} else if c > 1 {
+				sb.WriteString(strconv.Itoa(c))
+			} else if c == 1 {
+				sb.WriteRune(char(bm[p][0].dir))
+			} else if exp[p] {
+				sb.WriteRune('E')
+			} else {
+				sb.WriteRune('.')
+			}
+		}
+		sb.WriteRune('\n')
+	}
+	fmt.Println(sb.String())
+}
+
+func char(dir int) rune {
+	if dir == -1 {
+		return '<'
+	}
+	if dir == 1 {
+		return '>'
+	}
+	if dir < 0 {
+		return '^'
+	}
+	return 'v'
+}

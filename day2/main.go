b8fc0cada9ea638d3575ca988f99c92c223a28f1
Herman <herman@q42.nl>
2023-12-05 09:48:42 +0100

    part2

--- day2/main.go
+++ day2/main.go
@@ -7,7 +7,10 @@ import (
 	"strings"
 )
 
-type Game = []map[string]int
+type Game = []Round
+type Round struct {
+	R, G, B int
+}
 
 var maxRed = 12
 var maxGreen = 13
@@ -15,6 +18,7 @@ var maxBlue = 14
 
 func main() {
 	sum := 0
+	powerSum := 0
 	games := []Game{}
 	lib.EachLine(func(line string) {
 		line = line[strings.Index(line, ":")+2:]
@@ -25,33 +29,53 @@ func main() {
 		game := Game{}
 		for _, set := range sets {
 			colors := strings.Split(set, ", ")
+			round := Round{}
 			for _, color := range colors {
 				parts := strings.Split(color, " ")
 				digit, _ := strconv.Atoi(parts[0])
-				game = append(game, map[string]int{parts[1]: digit})
+				round.Set(parts[1], digit)
 			}
+			game = append(game, round)
 		}
 		games = append(games, game)
 		log.Println(game)
+		minimum := Round{}
 		for _, set := range game {
-			for color, digit := range set {
-				switch color {
-				case "red":
-					if digit > maxRed {
-						return
-					}
-				case "blue":
-					if digit > maxBlue {
-						return
-					}
-				case "green":
-					if digit > maxGreen {
-						return
-					}
-				}
+			if minimum.R < set.R {
+				minimum.R = set.R
+			}
+			if minimum.G < set.G {
+				minimum.G = set.G
+			}
+			if minimum.B < set.B {
+				minimum.B = set.B
+			}
+		}
+		powerSum += minimum.R * minimum.G * minimum.B
+		for _, set := range game {
+			if set.R > maxRed {
+				return
+			}
+			if set.G > maxGreen {
+				return
+			}
+			if set.B > maxBlue {
+				return
 			}
 		}
 		sum += len(games)
 	})
 	log.Println(sum)
+	log.Println(powerSum)
+}
+
+func (r *Round) Set(str string, digit int) {
+	switch str {
+	case "red":
+		r.R = digit
+	case "green":
+		r.G = digit
+	case "blue":
+		r.B = digit
+	}
 }

dc12255eceb593f1b63e6eef4424a1e48f0a91e4
Herman <herman@q42.nl>
2023-12-06 09:00:26 +0100

    day5p2 fails

--- day5/main.go
+++ day5/main.go
@@ -22,6 +22,7 @@ func main() {
 
 	scanner.Scan()
 	seeds := lib.Map(strings.Fields((scanner.Text()))[1:], lib.Int)
+
 	conv = make([][]Range, 0)
 	fmt.Println(seeds)
 	var Map []Range
@@ -34,21 +35,26 @@ func main() {
 			Map = make([]Range, 0)
 		} else if strings.TrimSpace(t) != "" {
 			c := lib.Map(strings.Fields(t), lib.Int)
-			Map = append(Map, Range{c[0], c[1], c[2]})
+			Map = append(Map, Range{Dst: c[0], Src: c[1], Len: c[2]})
 		}
+		sort.SliceStable(Map, func(i, j int) bool { return Map[i].Src < Map[j].Src })
 	}
 	conv = append(conv, Map)
 	fmt.Println(conv)
 
 	locations := []int{}
-	for _, seed := range seeds {
-		data := seed
-		// fmt.Println("seed", data)
-		for i := 0; i < len(conv); i++ {
-			// fmt.Println(" ", data, lookup(data, i))
-			data = lookup(data, i)
+	for i := 0; i < len(seeds); i += 2 {
+		start := seeds[i]
+		count := seeds[i+1]
+		for j := start; j < start+count; j++ {
+			data := j
+			fmt.Println("seed", data)
+			for i := 0; i < len(conv); i++ {
+				fmt.Println(" ", data, lookup(data, i))
+				data = lookup(data, i)
+			}
+			locations = append(locations, data)
 		}
-		locations = append(locations, data)
 	}
 	fmt.Println(locations)
 	sort.Ints(locations)
@@ -56,10 +62,14 @@ func main() {
 }
 
 func lookup(nr int, mapIdx int) int {
-	for _, r := range conv[mapIdx] {
-		if nr >= r.Src && nr <= r.Src+r.Len {
-			return nr + r.Dst - r.Src
-		}
+	// go find in sorted list
+	idx := sort.Search(len(conv[mapIdx]), func(i int) bool { return conv[mapIdx][i].Src >= nr })
+	if idx == len(conv[mapIdx]) {
+		return nr
+	}
+	r := conv[mapIdx][idx]
+	if nr <= r.Src+r.Len {
+		return nr + r.Dst - r.Src
 	}
 	return nr
 }

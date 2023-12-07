d6c3cbf2c7d0edf204949c207c1dbed9b68eabc7
Herman <herman@q42.nl>
2023-12-07 11:18:40 +0100

    day7p2

--- day7/main.go
+++ day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc/lib"
 	"fmt"
+	"slices"
 	"sort"
 	"strings"
 )
@@ -11,64 +12,112 @@ type Hand struct {
 	hand  string
 	cards [5]int
 	bid   int
+	cache [1]int
 }
 
 func (h Hand) String() string {
 	return h.hand
 }
 
-func (h Hand) Type() int {
-	sort.Sort(sort.IntSlice(h.cards[:]))
+func (h Hand) Options() []Hand {
+	if !slices.Contains(h.cards[:], int('J')) {
+		return []Hand{h}
+	}
+	return nil
+}
+
+func (h *Hand) Type() (out int) {
+	if h.cache[0] != 0 {
+		return h.cache[0]
+	}
+	defer func() {
+		h.cache[0] = out
+	}()
+
+	sort.Ints(h.cards[:])
 	var counts = map[int]int{}
+	jokers := 0
 	for _, c := range h.cards {
 		counts[c]++
+		if c == int('J') {
+			jokers++
+		}
 	}
-	countValues := []int{}
-	for _, count := range counts {
-		countValues = append(countValues, count)
+
+	countValues := [][2]int{}
+	for key, count := range counts {
+		countValues = append(countValues, [2]int{count, key})
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(countValues)))
-	if countValues[0] == 5 || countValues[0] == 4 {
-		return countValues[0] + 1 // five/four of a kind
+	sort.Slice(countValues, func(i, j int) bool {
+		return countValues[i][0] > countValues[j][0]
+	})
+
+	// apply jokers
+	if jokers > 0 {
+		for idx := range countValues {
+			if countValues[idx][1] != int('J') {
+				if h.hand == "JJJJ8" {
+					fmt.Println("bumping", string(rune(countValues[idx][1])), jokers)
+				}
+				countValues[idx][0] += jokers
+				jokers = 0
+				for jidx := range countValues {
+					if countValues[jidx][1] == int('J') {
+						countValues[jidx][0] = 0
+						break
+					}
+				}
+				break
+			}
+		}
 	}
-	if len(countValues) >= 2 && countValues[0] == 3 && countValues[1] == 2 {
+	// resort after applying jokers
+	sort.Slice(countValues, func(i, j int) bool {
+		return countValues[i][0] > countValues[j][0]
+	})
+
+	if countValues[0][0] == 5 || countValues[0][0] == 4 {
+		return countValues[0][0] + 1 // five/four of a kind
+	}
+	if len(countValues) >= 2 && countValues[0][0] == 3 && countValues[1][0] == 2 {
 		return 4 // full house
 	}
-	if countValues[0] == 3 {
+	if countValues[0][0] == 3 {
 		return 3 // three of a kind
 	}
-	if len(countValues) >= 2 && countValues[0] == 2 && countValues[1] == 2 {
+	if len(countValues) >= 2 && countValues[0][0] == 2 && countValues[1][0] == 2 {
 		return 2 // two pair
 	}
-	if countValues[0] == 2 {
+	if countValues[0][0] == 2 {
 		return 1 // one pair
 	}
-	return 0 // high card
+	return -1 // high card
 }
 
 func main() {
 	in := lib.Lines()
-	hands := lib.Map(in, func(s string) (out Hand) {
+	hands := lib.Map(in, func(s string) (out *Hand) {
 		parts := strings.Fields(s)
-		out.hand = parts[0]
+		out = &Hand{hand: parts[0], bid: lib.Int(parts[1])}
 		out.cards[0] = int(byte(parts[0][0]))
 		out.cards[1] = int(byte(parts[0][1]))
 		out.cards[2] = int(byte(parts[0][2]))
 		out.cards[3] = int(byte(parts[0][3]))
 		out.cards[4] = int(byte(parts[0][4]))
-		out.bid = lib.Int(parts[1])
 		return
 	})
 	sort.Sort(Hands(hands))
 	winnings := 0
 	for i, h := range hands {
-		fmt.Println(h.hand, i+1, h.bid)
+		fmt.Println(h.hand, i+1, h.bid, h.Type())
 		winnings += h.bid * (i + 1)
 	}
 	fmt.Println(winnings)
+	// 248055134
+	// 248860925
 }
 
-type Hands []Hand
+type Hands []*Hand
 
 // Len implements sort.Interface.
 func (h Hands) Len() int {
@@ -98,4 +147,4 @@ func (h Hands) Swap(i int, j int) {
 
 var _ sort.Interface = Hands{}
 
-var order = "23456789TJQKA"
+var order = "J23456789TQKA"

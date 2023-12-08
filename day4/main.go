767dc759ade076a1dc51a7ec2e16c4127132fc45
Herman <herman@q42.nl>
2023-12-08 09:33:19 +0100

    day4 cleanup

--- day4/main.go
+++ day4/main.go
@@ -18,11 +18,9 @@ func main() {
 	for i, card := range cards {
 		card = strings.TrimPrefix(strings.TrimSpace(strings.TrimPrefix(card, "Card")), strconv.Itoa(i+1)+":")
 		parts := strings.Split(card, "|")
-		fmt.Println(parts)
 		winners := strings.Fields(parts[0])
 		have := strings.Fields(parts[1])
 		overlap := len(lib.Intersect(lib.Map(winners, lib.Int), lib.Map(have, lib.Int)))
-		fmt.Println("overlap", overlap)
 		matches[i] = overlap
 		if overlap > 0 {
 			sum += 1 << (overlap - 1)
@@ -32,7 +30,6 @@ func main() {
 
 	for i := range cards {
 		for j := i + 1; j < i+1+matches[i]; j++ {
-			fmt.Printf("adding %d cards of %d\n", cardCounts[i], i+1)
 			cardCounts[j] += cardCounts[i]
 		}
 	}

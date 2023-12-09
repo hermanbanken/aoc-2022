07cdd099b1b9f7dfe68711a763bba654731d0183
Herman <herman@q42.nl>
2023-12-09 23:04:49 +0100

    cleanup

--- day9/main.go
+++ day9/main.go
@@ -10,20 +10,24 @@ func main() {
 	histories := lib.Map(lib.Lines(), func(line string) []int {
 		return lib.Map(strings.Fields(line), lib.Int)
 	})
-	// fmt.Println(lib.Sum(lib.Map(histories, part1head)))
-	fmt.Println(lib.Sum(lib.Map(histories, part2head)))
+	fmt.Println(lib.Sum(lib.Map(histories, setting{1}.Run)))
+	fmt.Println(lib.Sum(lib.Map(histories, setting{2}.Run)))
 }
 
-func part1head(history []int) (next int) {
-	fmt.Println()
-	return part1(history)
+type setting struct {
+	part int
 }
 
-func part1(history []int) (next int) {
-	fmt.Println(history)
-	defer func() {
-		fmt.Println(history, next)
-	}()
+func (s setting) Run(history []int) (next int) {
+	// fmt.Println()
+	return s.run(history)
+}
+
+func (s setting) run(history []int) (next int) {
+	// fmt.Println(history)
+	// defer func() {
+	// 	fmt.Println(history, next)
+	// }()
 	diff := make([]int, len(history)-1)
 	var allZero bool = true
 	for i := 0; i < len(history); i++ {
@@ -38,33 +42,10 @@ func part1(history []int) (next int) {
 		return 0
 	}
 
-	next = part1(diff)
-	return history[len(history)-1] + next
-}
-
-func part2head(history []int) (next int) {
-	fmt.Println()
-	return part2(history)
-}
-func part2(history []int) (next int) {
-	fmt.Println(history)
-	defer func() {
-		fmt.Println(history, next)
-	}()
-	diff := make([]int, len(history)-1)
-	var allZero bool = true
-	for i := 0; i < len(history); i++ {
-		if i < len(history)-1 {
-			diff[i] = history[i+1] - history[i]
-		}
-		if history[i] != 0 {
-			allZero = false
-		}
+	next = s.run(diff)
+	if s.part == 1 {
+		return history[len(history)-1] + next
+	} else {
+		return history[0] - next
 	}
-	if allZero {
-		return 0
-	}
-
-	next = part2(diff)
-	return history[0] - next
 }

9b7e61ec347580000d837880fd9df45ee039618c
Herman <herman@q42.nl>
2023-12-09 22:56:10 +0100

    day9p1

--- day9/main.go
+++ day9/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "aoc/lib"
+import (
+	"aoc/lib"
+	"fmt"
+	"strings"
+)
 
 func main() {
-	_ = lib.Lines()
+	histories := lib.Map(lib.Lines(), func(line string) []int {
+		return lib.Map(strings.Fields(line), lib.Int)
+	})
+	fmt.Println(lib.Sum(lib.Map(histories, part1head)))
+}
+
+func part1head(history []int) (next int) {
+	fmt.Println()
+	return part1(history)
+}
+
+func part1(history []int) (next int) {
+	fmt.Println(history)
+	defer func() {
+		fmt.Println(history, next)
+	}()
+	diff := make([]int, len(history)-1)
+	var allZero bool = true
+	for i := 0; i < len(history); i++ {
+		if i < len(history)-1 {
+			diff[i] = history[i+1] - history[i]
+		}
+		if history[i] != 0 {
+			allZero = false
+		}
+	}
+	if allZero {
+		return 0
+	}
+
+	next = part1(diff)
+	return history[len(history)-1] + next
 }

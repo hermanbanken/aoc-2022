89f0ad4ad587b5b345ae7a56f9ab0eef2374deed
Herman <herman@q42.nl>
2023-12-15 11:52:30 +0100

    day14p2

--- day14/main.go
+++ day14/main.go
@@ -4,6 +4,7 @@ import (
 	"aoc/lib"
 	"fmt"
 	"sort"
+	"strings"
 )
 
 type Tile [2]int
@@ -17,48 +18,149 @@ func weight(c int) int {
 	case int('.'):
 		return 0
 	}
-	panic("invalid")
+	panic(fmt.Sprintf("invalid %v", rune(c)))
 }
 
 func (tile Tile) Less(other Tile) bool {
-	if tile[0] == other[0] {
-		return weight(tile[1]) > weight(other[1])
-	}
-	return tile[0] < other[0]
+	return Less(tile, other)
 }
+
+func Less(tile Tile, other Tile) bool {
+	if tile[1] == other[1] {
+		return weight(tile[0]) > weight(other[0])
+	}
+	return tile[1] < other[1]
+}
+
+func weighM(m Matrix[Tile]) (sum int) {
+	height := len(m.data) / m.w
+	m.ForEach(func(x, y int, t Tile) {
+		if t[0] == 'O' {
+			sum += height - y
+		}
+	})
+	return sum
+}
+func print(t Tile) byte { return byte(t[0]) }
 
 func main() {
 	lines := lib.Lines()
-	stacks := make([][]Tile, len(lines[0]))
-	for _, line := range lib.Lines() {
-		for x := range line {
-			var last Tile
-			if len(stacks[x]) > 0 {
-				last = stacks[x][len(stacks[x])-1]
-			}
-			rank := last[0] + lib.Ternary(int(line[x]) == '#' || last[1] == '#', 1, 0)
-			stacks[x] = append(stacks[x], Tile{rank, int(line[x])})
-		}
+	m := Matrix[Tile]{
+		data: lib.Map([]byte(strings.Join(lines, "")), func(b byte) Tile { return Tile{int(b), 0} }),
+		w:    len(lines[0]),
 	}
-	// tilt north
-	sum := 0
-	for _, stack := range stacks {
-		// fmt.Println(string(lib.Map(stack, func(t Tile) rune { return rune(t[1]) })))
-		// fmt.Println(string(lib.Map(stack, func(t Tile) rune { return rune(t[0] + '0') })))
-		sort.SliceStable(stack, func(i, j int) bool {
-			return stack[i].Less(stack[j])
-		})
-		// fmt.Println(string(lib.Map(stack, func(t Tile) rune { return rune(t[1]) })))
-		// fmt.Println(string(lib.Map(stack, func(t Tile) rune { return rune(t[0] + '0') })))
+	var index = map[string]int{}
+	for cycle := 0; cycle < 1_000_000_000; cycle++ {
+		if cycle%1_000 == 0 {
+			fmt.Println(cycle)
+		}
+		// fmt.Println(m.String(print))
 		// fmt.Println()
-		stackSum := 0
-		for y := range stack {
-			if stack[y][1] == 'O' {
-				stackSum += len(stack) - y
+		for i := 0; i < 4; i++ {
+			tiltM(m)
+			if cycle == 0 && i == 0 {
+				fmt.Println("part1m", weighM(m), "\n ")
 			}
+			m.RotateCW()
 		}
-		fmt.Println(stackSum)
-		sum += stackSum
+		fmt.Println(weighM(m))
+		if first, hasIndex := index[m.String(print)]; hasIndex && (1_000_000_000-1-cycle)%(cycle-first) == 0 {
+			fmt.Println(first, "repeats at", cycle)
+			fmt.Println("part2", weighM(m))
+			return
+		}
+		index[m.String(print)] = cycle
+
 	}
-	fmt.Println("part1", sum)
+}
+
+func (m Matrix[T]) String(fn func(T) byte) string {
+	str := strings.Builder{}
+	for i := 0; i < len(m.data); i++ {
+		str.WriteByte(fn(m.data[i]))
+		if i%m.w == m.w-1 {
+			str.WriteRune('\n')
+		}
+	}
+	return str.String()
+}
+
+type Matrix[T any] struct {
+	data   []T
+	buffer []T
+	w      int
+}
+
+func (m *Matrix[T]) RotateCW() {
+	// x=2,y=0 => x=1,y=2
+	// x=2,y=1 => x=0,y=2
+	bw := len(m.data) / m.w
+	if m.buffer == nil {
+		m.buffer = make([]T, len(m.data))
+	}
+	for i := 0; i < len(m.data); i++ {
+		x := i % m.w
+		y := i / m.w
+		by := x
+		bx := bw - 1 - y
+		m.buffer[bx+by*bw] = m.data[i]
+	}
+	m.w = bw
+	copy(m.data, m.buffer)
+}
+
+func (m Matrix[T]) ForEach(fn func(x, y int, t T)) {
+	for i := 0; i < len(m.data); i++ {
+		fn(i%m.w, i/m.w, m.data[i])
+	}
+}
+
+func (m Matrix[T]) Get(x, y int) T {
+	return m.data[x+y*m.w]
+}
+func (m Matrix[T]) Set(x, y int, t T) {
+	m.data[x+y*m.w] = t
+}
+
+func (m Matrix[T]) SortColumn(i int, lessFn func(a, b T) bool) (out sort.Interface) {
+	return col[T]{m, i, lessFn}
+}
+
+type col[T any] struct {
+	m      Matrix[T]
+	col    int
+	lessFn func(a, b T) bool
+}
+
+// Len implements sort.Interface.
+func (col col[T]) Len() int {
+	return len(col.m.data) / col.m.w
+}
+
+// Less implements sort.Interface.
+func (col col[T]) Less(i int, j int) bool {
+	return col.lessFn(col.m.Get(col.col, i), col.m.Get(col.col, j))
+}
+
+// Swap implements sort.Interface.
+func (col col[T]) Swap(i int, j int) {
+	tmp := col.m.Get(col.col, i)
+	col.m.Set(col.col, i, col.m.Get(col.col, j))
+	col.m.Set(col.col, j, tmp)
+}
+
+func tiltM(m Matrix[Tile]) {
+	// rank stacks
+	m.ForEach(func(x, y int, t Tile) {
+		if y > 0 {
+			above := m.Get(x, y-1)
+			m.Set(x, y, Tile{t[0], above[1] + lib.Ternary(t[0] == '#' || above[0] == '#', 1, 0)})
+		} else {
+			m.Set(x, y, Tile{t[0], 0})
+		}
+	})
+	// tilt north
+	for i := 0; i < m.w; i++ {
+		sort.Stable(m.SortColumn(i, Less))
+	}
 }
